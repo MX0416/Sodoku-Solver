c4c6da774ce1b65a5eb6c90a7392343c29b9fa91
Matthew Xue <matthew.xue2004@gmail.com>
2024-08-15 22:00:46 -0700

    finished main file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# helper function to check if current move is valid
 def is_valid_move(grid, row, col, number): 
   # check row
   for x in range(9):
@@ -5,11 +6,38 @@ def is_valid_move(grid, row, col, number):
       return False
   # check col
   for x in range (9):
-    if grid(x)[col] == number:
+    if grid[x][col] == number:
       return False
-    
+  # check current 3x3 grid
   corner_row = row - row % 3
   corner_col = col - col % 3
+  for x in range(3):
+    for y in range(3):
+      if grid[corner_row + x][corner_col + y] == number:
+        return False
+  
+  return True
+
+def solve(grid, row, col):
+  if col == 9:
+    if row == 8: 
+      return True
+    row += 1
+    col = 0 
+
+  if grid[row][col] > 0:
+    return solve(grid, row, col + 1)
+  
+  for num in range (1, 10): 
+    if is_valid_move(grid, row, col, num):
+      grid[row][col] = num
+
+      if solve(grid, row, col + 1):
+        return True
+      
+    grid[row][col] = 0
+
+  return False
 
 
 grid = [[0,0,0,0,0,0,6,8,0],
@@ -21,3 +49,12 @@ grid = [[0,0,0,0,0,0,6,8,0],
         [9,6,0,0,0,0,3,0,8],
         [7,0,0,6,8,0,0,0,0],
         [0,2,8,0,0,0,0,0,0]]
+
+
+if solve(grid, 0, 0):
+  for i in range(9):
+    for j in range(9):
+      print(grid[i][j], end = " ")
+    print()
+else:
+  print("No possible solution!")
